3a637f74eb0806f85e2dda68e70eb0a463d93155
bruno_senigalha <brsenigalha@gmail.com>
2024-08-12 18:25:19 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -62,8 +62,8 @@ public class SellerDaoJDBC implements SellerDao {
 
         try {
             st = conn.prepareStatement("UPDATE seller " +
-                            "SET Name = ?,Email = ?,BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
-                            "WHERE Id = ?");
+                    "SET Name = ?,Email = ?,BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
+                    "WHERE Id = ?");
 
             st.setString(1, obj.getName());
             st.setString(2, obj.getEmail());
@@ -81,7 +81,22 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 
+            st.setInt(1, id);
+            int rows = st.executeUpdate();
+
+            if (rows == 0){
+                throw new DbException("Id not found.");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
