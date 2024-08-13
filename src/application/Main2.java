b51429db75fe7e74c42b77ae1bad5b784a8ef861
bruno_senigalha <brsenigalha@gmail.com>
2024-08-13 10:34:54 -0300

    department insert implementation

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -15,16 +15,22 @@ public class Main2 {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("=== TEST 1 : seller findAll ===");
+        System.out.println("=== TEST 1 : department findAll ===");
         List<Department> listDep = departmentDao.findAll();
         for (Department dep : listDep) {
             System.out.println(dep);
         }
 
-        System.out.println("\n=== TEST 2 : seller findById ===");
+        System.out.println("\n=== TEST 2 : department findById ===");
         System.out.println("Enter for id to find test: ");
         int id = sc.nextInt();
         Department dep = departmentDao.findById(id);
         System.out.println(dep);
+
+        System.out.println("\n=== TEST 3 : department insert===");
+        System.out.println("Enter the name of department to insert");
+        String depName = sc.next();
+        dep = new Department(null, depName);
+        departmentDao.insert(dep);
     }
 }
