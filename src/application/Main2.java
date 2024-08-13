bd805d4b3c15c03cf5b44c8070a01715791375d7
bruno_senigalha <brsenigalha@gmail.com>
2024-08-13 10:08:52 -0300

    Department findById implementation

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -6,9 +6,12 @@ import model.dao.impl.DepartmentDaoJDBC;
 import model.entities.Department;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
@@ -17,5 +20,11 @@ public class Main2 {
         for (Department dep : listDep) {
             System.out.println(dep);
         }
+
+        System.out.println("\n=== TEST 2 : seller findById ===");
+        System.out.println("Enter for id to find test: ");
+        int id = sc.nextInt();
+        Department dep = departmentDao.findById(id);
+        System.out.println(dep);
     }
 }
